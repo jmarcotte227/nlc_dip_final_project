26460ab5c335281d014d082c57a7ed29c371146d
DChen2024 <davidchen0024@gmail.com>
2025-04-09 16:03:45 -0400

    Upload model

---
+++ model.m
@@ -0,0 +1,31 @@
+m1=1; r1=0.5; l1=1; I1=1/3*m1*l1^2;
+m2=1; r2=0.5; l2=1; I2=1/3*m2*l2^2;
+g = 9.81;
+
+a11=I1+m2*l1;
+a12=m2*r2*l1;
+a22=I2;
+b1=(m1*r1+m2*l1)*g;
+b2=m2*r2*g;
+
+[t,y] = ode45(@(t,x) odefun(t,x,a11,a12,a22,b1,b2), [0, 10], [pi; pi; 0; 0]);
+plot(t,y(:,1:2))
+
+function dxdt=odefun(t,x,a11,a12,a22,b1,b2)
+
+A=[a11, a12*cos(x(2)-x(1));
+   a12*cos(x(2)-x(1)), a22];
+
+F=[0, -a12*sin(x(2)-x(1));
+   a12*sin(x(2)-x(1)), 0];
+
+B=[-b1, 0; 0, -b2];
+
+C=[1, 0; -1, 1];
+
+M=[1; 0];
+
+rhs = C*M - F*[x(3)^2; x(4)^2] - B*[sin(x(1)); sin(x(2))];
+accel = A\rhs;
+dxdt = [x(3); x(4); accel(1); accel(2)];
+end
