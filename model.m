dfdfcb0287ad36b8c6b32868077a9ce43d692d2e
DChen2024 <davidchen0024@gmail.com>
2025-04-14 17:05:08 -0400

    Controller design

--- model.m
+++ model.m
@@ -1,31 +1,89 @@
-m1=1; r1=0.5; l1=1; I1=1/3*m1*l1^2;
-m2=1; r2=0.5; l2=1; I2=1/3*m2*l2^2;
-g = 9.81;
-
-a11=I1+m2*l1;
-a12=m2*r2*l1;
-a22=I2;
-b1=(m1*r1+m2*l1)*g;
-b2=m2*r2*g;
-
-[t,y] = ode45(@(t,x) odefun(t,x,a11,a12,a22,b1,b2), [0, 10], [pi; pi; 0; 0]);
+options = odeset('OutputFcn', @outfun);
+[t,y] = ode45(@odefun, [0, 10], [pi; pi; 0; 0], options);
+figure(1)
 plot(t,y(:,1:2))
+xlabel('t (s)')
+ylabel('\theta (rad)')
+title('State Variables')
+legend('\theta_1', '\theta_2')
+figure(2)
+plot(t,u)
+xlabel('t (s)')
+ylabel('u (N*m)')
+title('Control Effort')
+legend('u_1', 'u_2')
 
-function dxdt=odefun(t,x,a11,a12,a22,b1,b2)
+% Model simulation
+function dxdt=odefun(t,x)
+    [A, F, B, C] = matrices(x);
+    U = control_outer(x, A, F, B, C);
 
-A=[a11, a12*cos(x(2)-x(1));
-   a12*cos(x(2)-x(1)), a22];
+    rhs = C*U - F*[x(3)^2; x(4)^2] - B*[sin(x(1)); sin(x(2))];
+    accel = A\rhs;
+    dxdt = [x(3); x(4); accel(1); accel(2)];
+end
 
-F=[0, -a12*sin(x(2)-x(1));
-   a12*sin(x(2)-x(1)), 0];
+% Additionally save the input
+function status=outfun(t,x,flag)
+    persistent u % declare as static variable
+    if strcmp(flag, 'done')
+        assignin('base', 'u', u); % save to base workspace
+        return;
+    end
+    if strcmp(flag, 'init')
+        [A, F, B, C] = matrices(x);
+        U = control_outer(x, A, F, B, C);
+        u = U';
+    else
+        for i=1:4 % artifact of using ode45
+            [A, F, B, C] = matrices(x(:,i));
+            U = control_outer(x(:,i), A, F, B, C);
+            u = [u; U'];
+        end
+    end
+    status = 0; % return 0 to continue
+end
 
-B=[-b1, 0; 0, -b2];
+% Decoupled second order system
+function V=control_inner(x)
+    zeta1=1;
+    omega1=1;
+    zeta2=1;
+    omega2=1;
+    
+    k11=omega1^2;
+    k21=2*zeta1*omega1;
+    k12=omega2^2;
+    k22=2*zeta2*omega2;
+    
+    V = [-k11*x(1)-k21*x(3); -k12*x(2)-k22*x(4)];
+end
 
-C=[1, 0; -1, 1];
+% Wrap the inner controller
+function U=control_outer(x, A, F, B, C)
+    U=C\A*(control_inner(x) + A\(F*[x(3)^2; x(4)^2]+B*[sin(x(1)); sin(x(2))]));
+end
 
-M=[1; 0];
+% Calculate matrices
+function [A, F, B, C] = matrices(x)
+    % Model parameters
+    m1=1; r1=0.5; l1=1; I1=1/3*m1*l1^2;
+    m2=1; r2=0.5; l2=1; I2=1/3*m2*l2^2;
+    g = 9.81;
+    
+    a11=I1+m2*l1^2;
+    a12=m2*r2*l1;
+    a22=I2;
+    b1=(m1*r1+m2*l1)*g;
+    b2=m2*r2*g;
 
-rhs = C*M - F*[x(3)^2; x(4)^2] - B*[sin(x(1)); sin(x(2))];
-accel = A\rhs;
-dxdt = [x(3); x(4); accel(1); accel(2)];
+    A=[a11, a12*cos(x(2)-x(1));
+       a12*cos(x(2)-x(1)), a22];
+    
+    F=[0, -a12*sin(x(2)-x(1));
+       a12*sin(x(2)-x(1)), 0];
+    
+    B=[-b1, 0; 0, -b2];
+    
+    C=[1, -1; 0, 1];
 end
